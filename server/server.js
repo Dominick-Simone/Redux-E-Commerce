5a6861c9f3eb54a2519fbf4f9d0fbaccdaaaa2aa
Dominick Simone <domsimone123@gmail.com>
2021-11-06 10:50:02 -0400

    fixing stuff

--- server/server.js
+++ server/server.js
@@ -20,14 +20,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
+app.use('/images', express.static(path.join(__dirname, '../react-ui/images')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../react-ui/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../react-ui/build/index.html'));
 });
 
 db.once('open', () => {
