db37bd4072327cfa54d099792ccf35cf9b92c817
Dominick Simone <domsimone123@gmail.com>
2021-11-06 10:07:07 -0400

    final commit

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,11 @@
+import React, { createContext, useContext } from "react";
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+export default function StoreProvider( props ) {
+
+
+  return <Provider store={store} {...props} />;
+};
+
